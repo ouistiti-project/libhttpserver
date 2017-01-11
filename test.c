87196f4d35a7bf259cb70e0561b87febbd2ba4e9
Marc Chalain <marc.chalain@smile.fr>
2017-01-11 09:35:37 +0100

    [test]  send the file content

--- test.c
+++ test.c
@@ -28,6 +28,7 @@
 #ifdef HTTPSERVER
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #ifndef WIN32
 # include <pwd.h>
@@ -46,21 +47,84 @@ int client = 0;
 #include "mod_mbedtls.h"
 #endif
 
-int test_func1(void *arg, http_message_t *request, http_message_t *response)
-{
-	char content[] = "<html><head><link rel=\"stylesheet\" href=\"styles.css\"></head><body>coucou<br/></body></html>";
-	httpmessage_addheader(response, "Server", "libhttpserver");
-	httpmessage_addcontent(response, "text/html", content, strlen(content));
-	return 0;
-}
+#define TEST_FILE
 
-int test_func2(void *arg, http_message_t *request, http_message_t *response)
+char test_content[] = "<html><head><link rel=\"stylesheet\" href=\"styles.css\"></head><body>coucou<br/></body></html>";
+#ifdef TEST_STREAMING
+struct test_config_s
+{
+	int unsused;
+};
+struct test_config_s *test_config = NULL;
+int test_func(void *arg, http_message_t *request, http_message_t *response)
 {
 	httpmessage_addheader(response, "Server", "libhttpserver");
 	httpmessage_addcontent(response, "text/html", NULL, 0);
 	client = httpmessage_keepalive(response);
 	return 0;
 }
+#elif defined(TEST_FILE)
+struct test_config_s
+{
+	char *rootdoc;
+	FILE *fileno;
+};
+struct test_config_s g_test_config = 
+{
+	.rootdoc = "/srv/www/htdocs",
+	.fileno = NULL,
+};
+struct test_config_s *test_config = &g_test_config;
+int test_func(void *arg, http_message_t *request, http_message_t *response)
+{
+	struct test_config_s *config = (struct test_config_s *)arg;
+	char content[64];
+	int size;
+	if (!config->fileno)
+	{
+		char filepath[512];
+		snprintf(filepath, 511, "%s%s", config->rootdoc, httpmessage_REQUEST(request, "uri"));
+		struct stat filestat;
+		int ret = stat(filepath, &filestat);
+		if (S_ISDIR(filestat.st_mode))
+		{
+			snprintf(filepath, 511, "%s%s/index.html", config->rootdoc, httpmessage_REQUEST(request, "uri"));
+			stat(filepath, &filestat);
+		}
+		if (ret != 0)
+		{
+			printf("file: %s not found\n", filepath);
+			return EREJECT;
+		}
+		printf("open file %d %s\n", filestat.st_size, filepath);
+			httpmessage_addheader(response, "Server", "libhttpserver");
+			config->fileno = fopen(filepath, "rb");
+	}
+	if (feof(config->fileno))
+	{
+		printf("end of file\n");
+		fclose(config->fileno);
+		config->fileno = NULL;
+		return ESUCCESS;
+	}
+	size = fread(content, 1, sizeof(content), config->fileno);
+	printf("read %d %d\n", size, sizeof(content));
+	httpmessage_addcontent(response, "text/html", content, size);
+	return ECONTINUE;
+}
+#else
+struct test_config_s
+{
+	int unsused;
+};
+struct test_config_s *test_config = NULL;
+int test_func(void *arg, http_message_t *request, http_message_t *response)
+{
+	httpmessage_addheader(response, "Server", "libhttpserver");
+	httpmessage_addcontent(response, "text/html", test_content, strlen(test_content));
+	return 0;
+}
+#endif
 
 int main(int argc, char * const *argv)
 {
@@ -74,13 +138,22 @@ int main(int argc, char * const *argv)
 #endif
 	setbuf(stdout, NULL);
 #ifdef MBEDTLS
-	void * mod = mod_mbedtls_create("/etc/ssl/private/server.pem",NULL,NULL);
+	mod_mbedtls_t mbedtlsconfig = 
+	{
+		.pers = "httpserver-mbedtls",
+		.crtfile = "/etc/ssl/private/server.pem",
+		.pemfile = NULL,
+		.cachain = NULL,
+		.dhmfile = "/etc/ssl/private/dhparam.pem",
+	};
+	void * mod = mod_mbedtls_create(&mbedtlsconfig);
 	config = mod_mbedtls_getmod(mod);
 #endif
 	http_server_t *server = httpserver_create(config);
 	if (server)
 	{
-		httpserver_addconnector(server, NULL, test_func1, NULL);
+		
+		httpserver_addconnector(server, NULL, test_func, test_config);
 #ifndef WIN32
 		if (user != NULL)
 		{
@@ -89,7 +162,7 @@ int main(int argc, char * const *argv)
 		}
 #endif
 		httpserver_connect(server);
-/*
+#ifdef TEST_STREAMING
 		char data[1550];
 		memset(data, 0xA5, sizeof(data));
 		char clock[4] = {'-','\\','|','/'};
@@ -110,7 +183,7 @@ int main(int argc, char * const *argv)
 				printf("send stream %d\n",ret);
 			}
 		}
-*/
+#endif
 		httpserver_disconnect(server);
 	}
 #ifdef MBEDTLS
