b0393d3c0e82eb3a70410e0dbea3787e357b62a0
Marc Chalain <marc.chalain@smile.fr>
2017-07-26 14:50:29 +0200

    [build] slice the libraries and the modules build rules

--- src/utils.mk
+++ src/utils.mk
@@ -1,5 +1,13 @@
-modules-$(DYNAMIC)+=utils_mod
-slib-$(STATIC)+=utils_mod
+ifeq ($(STATIC),y)
+SLIB_UTILS:=$(LIBUTILS)
+endif
+
+ifeq ($(DYNAMIC),y)
+DLIB_UTILS:=$(LIBUTILS)
+endif
+
+modules-$(DLIB_UTILS)+=utils_mod
+slib-$(SLIB_UTILS)+=utils_mod
 utils_mod_SOURCES=utils.c
 utils_mod_CFLAGS+=-I../include 
 
