6755dae9f8b352c227d0e2311547c89a7fe4e496
Marc Chalain <marc.chalain@smile.fr>
2018-03-07 08:42:21 +0100

    [mbedtls] useless to link libraries to a library

--- src/mod_mbedtls.mk
+++ src/mod_mbedtls.mk
@@ -1,7 +1,7 @@
 modules-$(DYNAMIC)+=mod_mbedtls
 slib-$(STATIC)+=mod_mbedtls
 mod_mbedtls_SOURCES+=mod_mbedtls.c
-mod_mbedtls_LIBRARY+=mbedtls mbedx509 mbedcrypto
+#mod_mbedtls_LIBRARY+=mbedtls mbedx509 mbedcrypto
 mod_mbedtls_CFLAGS+=-I../include/httpserver 
 
 mod_mbedtls_CFLAGS-$(DEBUG)+=-g -DDEBUG
