898b53f12e04a465a4d6544a5bbc50806ad014e4
Marc Chalain <marc.chalain@smile.fr>
2019-01-31 14:53:38 +0100

    [hash] add debug macro

--- src/hash_libb64.c
+++ src/hash_libb64.c
@@ -33,6 +33,14 @@
 # include "b64/cencode.h"
 # include "b64/cdecode.h"
 
+#define err(format, ...) fprintf(stderr, "\x1B[31m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#define warn(format, ...) fprintf(stderr, "\x1B[35m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#ifdef DEBUG
+#define dbg(format, ...) fprintf(stderr, "\x1B[32m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
 static void BASE64_encode(const char *in, int inlen, char *out, int outlen);
 static void BASE64_decode(const char *in, int inlen, char *out, int outlen);
 const base64_t *base64 = &(const base64_t)
--- src/hash_mbedtls.c
+++ src/hash_mbedtls.c
@@ -36,6 +36,14 @@
 #include "httpserver/hash.h"
 #include "httpserver/log.h"
 
+#define err(format, ...) fprintf(stderr, "\x1B[31m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#define warn(format, ...) fprintf(stderr, "\x1B[35m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#ifdef DEBUG
+#define dbg(format, ...) fprintf(stderr, "\x1B[32m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
 static void BASE64_encode(const char *in, int inlen, char *out, int outlen);
 static void BASE64_decode(const char *in, int inlen, char *out, int outlen);
 static void *MD5_init();
