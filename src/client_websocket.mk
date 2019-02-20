f643db4f0f2233e7c9746e26f6f0c58bbe387e41
Marc Chalain <marc.chalain@smile.fr>
2019-02-20 10:56:56 +0100

    [websocket] add TLS support on clien app

--- src/client_websocket.mk
+++ src/client_websocket.mk
@@ -2,9 +2,11 @@ bin-$(HTTPCLIENT_FEATURES)=client_websocket
 
 client_websocket_SOURCES+=client_websocket.c
 client_websocket_CFLAGS+=-I../include
+client_websocket_CFLAGS-$(MBEDTLS)+=-DMBEDTLS
 client_websocket_LDFLAGS+=-L$(obj)httpserver/
 client_websocket_LIBRARY+=pthread
 client_websocket_LIBS+=httpserver
 client_websocket_LIBS+=websocket
+client_websocket_LIBS-$(MBEDTLS)+=mbedtls mbedx509 mbedcrypto
 
 client_websocket_CFLAGS-$(DEBUG)+=-DDEBUG -g
