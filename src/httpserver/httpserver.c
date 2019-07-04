fa52f1cd295e019fa988a1f9d8a2cb27234954ac
mchalain <marc.chalain@gmail.com>
2019-07-04 08:50:35 +0200

    [client] reset the content buffer before sending

--- src/httpserver/httpserver.c
+++ src/httpserver/httpserver.c
@@ -954,7 +954,7 @@ int httpmessage_content(http_message_t *message, char **data, unsigned long long
 			//message->content = NULL;
 		}
 	}
-	if (message->state & GENERATE_MASK != 0)
+	if ((message->state & GENERATE_MASK) != 0)
 		return size;
 	if (state < PARSE_CONTENT)
 		return EINCOMPLETE;
@@ -1368,12 +1368,10 @@ int httpclient_sendrequest(http_client_t *client, http_message_t *request, http_
 		size = client->client_send(client->send_arg, "\r\n", 2);
 		ret = EINCOMPLETE;
 		request->state = GENERATE_CONTENT;
-
-		data = request->content;
-		data->offset = data->data;
 	break;
 	case GENERATE_CONTENT:
 		data = request->content;
+		data->offset = data->data;
 		while (data->length > 0)
 		{
 			/**
@@ -1396,7 +1394,6 @@ int httpclient_sendrequest(http_client_t *client, http_message_t *request, http_
 				request->content_length -= size;
 		}
 		ret = EINCOMPLETE;
-			request->state = GENERATE_END;
 		if (request->content_length <= 0)
 		{
 			request->state = GENERATE_END;
