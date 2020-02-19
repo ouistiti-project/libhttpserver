80154a4ce6477a19c40a3566f392f21fa6417194
mchalain <marc.chalain@smile.fr>
2020-02-19 08:19:56 +0100

    [build] move modules to ouistiti repository

--- src/mod_openssl.mk
+++
@@ -1,8 +0,0 @@
-modules-$(MODULES)+=mod_openssl
-slib-y+=mod_openssl
-mod_openssl_SOURCES+=mod_openssl.c
-mod_openssl_LIBRARY+=ssl crypto
-mod_openssl_CFLAGS+=-I../include
-mod_openssl_ALIAS-$(MODULES)+=mod_tls.so
-
-mod_openssl_CFLAGS-$(DEBUG)+=-g -DDEBUG
--- src/mod_wolfssl.mk
+++
@@ -1,8 +0,0 @@
-modules-$(MODULES)+=mod_wolfssl
-slib-y+=mod_wolfssl
-mod_wolfssl_SOURCES+=mod_wolfssl.c
-mod_wolfssl_CFLAGS+=-I../include/httpserver 
-mod_wolfssl_CFLAGS-$(MODULES)+=-DMODULES
-mod_wolfssl_ALIAS-$(MODULES)+=mod_tls.so
-
-mod_wolfssl_CFLAGS-$(DEBUG)+=-g -DDEBUG
