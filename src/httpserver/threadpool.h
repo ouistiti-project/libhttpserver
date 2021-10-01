fcdc7a07e9b1cc493bad448a35ab19ed5031eee7
Marc Chalain <marc.chalain@gmail.com>
2021-10-01 10:01:25 +0200

    [vthread] new threadpool API

---
+++ src/httpserver/threadpool.h
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * threadpool.h: pthread pool
+ * this file is part of https://github.com/ouistiti-project/libhttpserver
+ *****************************************************************************
+ * Copyright (C) 2021-2022
+ *
+ * Authors: Marc Chalain <marc.chalain@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+#ifndef __THREADPOOL_H__
+#define __THREADPOOL_H__
+
+typedef struct threadpool_s threadpool_t;
+typedef int(*threadhandler_t)(void *data, void *userdata);
+
+threadpool_t *threadpool_init(int pooldepth);
+int threadpool_get(threadpool_t *pool, ThreadHandler_t hdl, void *hdldata, void *userdata);
+void threadpool_destroy(threadpool_t *pool);
+
+#endif
