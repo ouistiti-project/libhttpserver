d7c93a60966ced5775d0a6981ae0f6a4a003dcc4
mchalain <marc.chalain@scantech.com>
2023-11-17 14:34:48 +0100

    hash: new hash API to extend Base64 encoding, decoding

--- include/ouistiti/hash.h
+++ include/ouistiti/hash.h
@@ -33,7 +33,19 @@ typedef struct base64_s base64_t;
 struct base64_s
 {
 	int (*encode)(const char *in, size_t inlen, char *out, size_t outlen);
+	struct{
+		void *(*init)();
+		size_t (*update)(void *ctx, unsigned char *out, const unsigned char *data, size_t length);
+		size_t (*finish)(void *ctx, unsigned char *out);
+		size_t (*length)(void *ctx, size_t inlen);
+	} encoder;
 	int (*decode)(const char *in, size_t inlen, char *out, size_t outlen);
+	struct{
+		void *(*init)();
+		size_t (*update)(void *ctx, unsigned char *out, const unsigned char *data, size_t length);
+		size_t (*finish)(void *ctx, unsigned char *out);
+		size_t (*length)(void *ctx, size_t inlen);
+	} decoder;
 };
 
 #ifdef SHA512
